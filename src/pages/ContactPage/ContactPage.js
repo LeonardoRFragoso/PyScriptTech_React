a5fb742f00e169a059d6e6b812bf93b48db2447d
Leonardo Fragoso <leonardorfragoso@gmail.com>
2024-10-12 18:43:58 -0300

    Ajustando Responsividade Mobile

--- src/pages/ContactPage/ContactPage.js
+++ src/pages/ContactPage/ContactPage.js
@@ -1,10 +1,47 @@
 // src/pages/ContactPage/ContactPage.js
-import React from 'react';
+import React, { useState } from 'react';
 import './ContactPage.css';  // Certifique-se de que o caminho está correto
 import whatsappIcon from '../../assets/Icons/whatsapp.png';  // Caminho correto para a imagem do WhatsApp
 import gmailIcon from '../../assets/Icons/gmail.png';  // Caminho correto para a imagem do Gmail
+import emailjs from 'emailjs-com';
 
 const ContactPage = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    phone: '',
+    description: '',
+  });
+  const [successMessage, setSuccessMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    emailjs.send('service_mpvslhm', 'template_EmailJS', {
+      user_name: formData.name,
+      user_email: formData.email,
+      user_phone: formData.phone,
+      user_description: formData.description,
+    }, 'Wok3mV-Bl-3UNJa9I')
+    .then((response) => {
+      console.log('Email enviado com sucesso!', response.status, response.text);
+      setSuccessMessage('Mensagem enviada com sucesso!');
+      setErrorMessage('');
+      // Resetar o formulário após o envio
+      setFormData({ name: '', email: '', phone: '', description: '' });
+    }, (err) => {
+      console.error('Falha ao enviar email:', err);
+      setErrorMessage('Falha ao enviar a mensagem. Tente novamente mais tarde.');
+      setSuccessMessage('');
+    });
+  };
+
   return (
     <div className="contactPage">  {/* Certifique-se de usar a classe CSS correta */}
       <h1 className="contactTitle">Contato</h1>
@@ -12,25 +49,53 @@ const ContactPage = () => {
         Entre em contato através do formulário abaixo ou clicando no ícone do Gmail para nos enviar um email.
       </p>
 
-      <form className="contactForm">
+      <form onSubmit={handleSubmit} className="contactForm">
         <div className="formGroup">
           <label htmlFor="name">Nome:</label>
-          <input type="text" id="name" name="name" required />
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
         </div>
 
         <div className="formGroup">
           <label htmlFor="email">Email:</label>
-          <input type="email" id="email" name="email" required />
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
         </div>
 
         <div className="formGroup">
           <label htmlFor="phone">Telefone:</label>
-          <input type="tel" id="phone" name="phone" required />
+          <input
+            type="tel"
+            id="phone"
+            name="phone"
+            value={formData.phone}
+            onChange={handleChange}
+            required
+          />
         </div>
 
         <div className="formGroup">
           <label htmlFor="description">Descrição do Projeto:</label>
-          <textarea id="description" name="description" rows="5" required />
+          <textarea
+            id="description"
+            name="description"
+            rows="5"
+            value={formData.description}
+            onChange={handleChange}
+            required
+          />
         </div>
 
         <button type="submit" className="submitButton">Enviar</button>
@@ -49,6 +114,9 @@ const ContactPage = () => {
           Enviar Email
         </a>
       </div>
+
+      {successMessage && <p className="successMessage">{successMessage}</p>}
+      {errorMessage && <p className="errorMessage">{errorMessage}</p>}
     </div>
   );
 };
--- src/pages/HomePage/ContactSection/ContactSection.js
+++ src/pages/HomePage/ContactSection/ContactSection.js
@@ -1,13 +1,17 @@
 // src/pages/HomePage/ContactSection/ContactSection.js
 import React, { useState } from 'react';
 import styles from './ContactSection.module.css';
+import emailjs from 'emailjs-com';
 
 const ContactSection = () => {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
+    phone: '', // Adicionando o campo telefone
     message: '',
   });
+  const [successMessage, setSuccessMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -16,47 +20,71 @@ const ContactSection = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Aqui você pode implementar a lógica para enviar os dados do formulário
-    console.log('Formulário enviado:', formData);
-    // Resetar o formulário após o envio
-    setFormData({ name: '', email: '', message: '' });
+    
+    emailjs.send('service_mpvslhm', 'template_EmailJS', {
+      user_name: formData.name,
+      user_email: formData.email,
+      user_phone: formData.phone, // Enviando o telefone para o EmailJS
+      user_message: formData.message,
+    }, 'Wok3mV-Bl-3UNJa9I')
+    .then((response) => {
+      console.log('Email enviado com sucesso!', response.status, response.text);
+      setSuccessMessage('Mensagem enviada com sucesso!');
+      setErrorMessage('');
+      // Resetar o formulário após o envio
+      setFormData({ name: '', email: '', phone: '', message: '' }); // Resetando o campo de telefone
+    }, (err) => {
+      console.error('Falha ao enviar email:', err);
+      setErrorMessage('Falha ao enviar a mensagem. Tente novamente mais tarde.');
+      setSuccessMessage('');
+    });
   };
 
   return (
     <section className={styles.contactSection}>
-    <h2>Entre em contato</h2>
-    <p>Se você está interessado em nossos serviços, não hesite em nos contatar!</p>
-    <form onSubmit={handleSubmit} className={styles.contactForm}>
-      <input
-        type="text"
-        name="name"
-        placeholder="Seu nome"
-        value={formData.name}
-        onChange={handleChange}
-        className={styles.inputField}
-        required
-      />
-      <input
-        type="email"
-        name="email"
-        placeholder="Seu e-mail"
-        value={formData.email}
-        onChange={handleChange}
-        className={styles.inputField}
-        required
-      />
-      <textarea
-        name="message"
-        placeholder="Sua mensagem"
-        value={formData.message}
-        onChange={handleChange}
-        className={styles.textareaField}
-        required
-      />
-      <button type="submit" className={styles.ctaButton}>Enviar</button>
-    </form>
-  </section>
-  
+      <h2>Entre em contato</h2>
+      <p>Se você está interessado em nossos serviços, não hesite em nos contatar!</p>
+      <form onSubmit={handleSubmit} className={styles.contactForm}>
+        <input
+          type="text"
+          name="name"
+          placeholder="Seu nome"
+          value={formData.name}
+          onChange={handleChange}
+          className={styles.inputField}
+          required
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="Seu e-mail"
+          value={formData.email}
+          onChange={handleChange}
+          className={styles.inputField}
+          required
+        />
+        <input
+          type="tel" // Definindo o tipo como telefone
+          name="phone"
+          placeholder="Seu telefone"
+          value={formData.phone}
+          onChange={handleChange}
+          className={styles.inputField}
+          required
+        />
+        <textarea
+          name="message"
+          placeholder="Sua mensagem"
+          value={formData.message}
+          onChange={handleChange}
+          className={styles.textareaField}
+          required
+        />
+        <button type="submit" className={styles.ctaButton}>Enviar</button>
+      </form>
+      {successMessage && <p className={styles.successMessage}>{successMessage}</p>}
+      {errorMessage && <p className={styles.errorMessage}>{errorMessage}</p>}
+    </section>
   );
 };
 
